a05f30634d1c8b90c8d23388fe5614d64d4a59fe
Yujin Won <iamyujinwon@gmail.com>
2022-07-26 15:17:08 -0400

    add css styles for login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styles from './login.module.css';
+import { BsGoogle, BsGithub } from 'react-icons/bs';
+import Character from '../../images/character.svg';
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <>
+      <img src={Character} alt='character' />
+      <section className={styles.loginCard}>
+        <div className={styles.login}>
+          <span>Login</span>
+          <p>Choose your login method</p>
+          <div className={styles.loginChoice}>
+            <div>
+              <BsGoogle />
+              <span>Google</span>
+            </div>
+            <div>
+              <BsGithub />
+              <span>GitHub</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Login;
