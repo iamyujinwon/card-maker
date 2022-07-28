d78b4ea4c9fd8359ba73e7a7549a2d3866a0718d
Yujin Won <iamyujinwon@gmail.com>
2022-07-28 18:56:10 -0400

    make ui for form design

--- src/components/card_edit_form/cardEditForm.jsx
+++ src/components/card_edit_form/cardEditForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../button/button';
+import ImageFileInput from '../image_file_input/imageFileInput';
 import styles from './cardEditForm.module.css';
 
 const CardEditForm = ({ card }) => {
@@ -8,18 +9,81 @@ const CardEditForm = ({ card }) => {
 
   const onSubmit = () => {};
   return (
-    <form>
-      <input type='text' name='name' value={name} />
-      <input type='text' name='company' value={company} />
-      <select name='theme' value={theme}>
-        <option value='dark'>Dark</option>
-        <option value='light'>Light</option>
-        <option value='colorful'>Colorful</option>
-      </select>
-      <input type='text' name='title' value={title} />
-      <input type='text' name='email' value={email} />
-      <textarea name='message' value={message}></textarea>
-      <Button name='Delete' onClick={onSubmit} />
+    <form className={styles.form}>
+      <section className={styles.section}>
+        <div>
+          <label className={styles.label} for='name'>
+            Name
+          </label>
+          <input
+            className={styles.input}
+            type='text'
+            name='name'
+            value={name}
+          />
+        </div>
+        <div>
+          <label className={styles.label} for='email'>
+            Email
+          </label>
+          <input
+            className={styles.input}
+            type='text'
+            name='email'
+            value={email}
+          />
+        </div>
+      </section>
+      <section className={styles.section}>
+        <div>
+          <label className={styles.label} for='company'>
+            Company
+          </label>
+          <input
+            className={styles.input}
+            type='text'
+            name='company'
+            value={company}
+          />
+        </div>
+        <div>
+          <label className={styles.label} for='title'>
+            Title
+          </label>
+          <input
+            className={styles.input}
+            type='text'
+            name='title'
+            value={title}
+          />
+        </div>
+        <div>
+          <label className={styles.label} for='theme'>
+            Theme
+          </label>
+          <select className={styles.select} name='theme' value={theme}>
+            <option value='dark'>Dark</option>
+            <option value='light'>Light</option>
+            <option value='colorful'>Colorful</option>
+          </select>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <div>
+          <label className={styles.label} for='message'>
+            Message
+          </label>
+          <textarea
+            className={styles.textarea}
+            name='message'
+            value={message}
+          ></textarea>
+        </div>
+      </section>
+      <div className={styles.buttons}>
+        <Button name='Delete' onClick={onSubmit} />
+        <ImageFileInput />
+      </div>
     </form>
   );
 };
---
+++ src/components/image_file_input/imageFileInput.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './imageFileInput.module.css';
+
+const ImageFileInput = (props) => {
+  return <button className={styles.button}>Submit</button>;
+};
+
+export default ImageFileInput;
