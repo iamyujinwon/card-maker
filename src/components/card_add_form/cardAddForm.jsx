0df92a27fa4fe7bb58326ac3a7aeca270224da76
Yujin Won <iamyujinwon@gmail.com>
2022-08-03 17:13:57 -0400

    clean warning

--- src/components/card_add_form/cardAddForm.jsx
+++ src/components/card_add_form/cardAddForm.jsx
@@ -13,8 +13,6 @@ const CardAddForm = ({ FileInput, onAdd }) => {
   const [file, setFile] = useState({ fileName: null, fileURL: null });
 
   const onFileChange = (file) => {
-    console.log(file);
-
     setFile({
       fileName: file.name,
       fileURL: file.url,
