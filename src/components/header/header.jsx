d46703113d0e0f6dcae90d5f7513b920220eb5f5
Yujin Won <iamyujinwon@gmail.com>
2022-08-04 19:02:43 -0400

    add Dropdown memu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,32 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import styles from './header.module.css';
 import logo from '../images/logo.svg';
+import { FiLogOut } from 'react-icons/fi';
 
 const Header = memo(({ onLogout, currentUserName }) => {
+  const [showDropDown, setShowDropDown] = useState(false);
+  const firstLetter = currentUserName[0];
+
+  const onClick = () => {
+    setShowDropDown(!showDropDown);
+  };
+
   return (
     <section className={styles.header}>
       <img className={styles.logo} src={logo} alt='logo' />
-      <div>{currentUserName}</div>
-      <button className={styles.button} onClick={onLogout}>
-        <span>Log out</span>
+      <button className={styles.profile} onClick={onClick}>
+        <div className={styles.displayName}>{currentUserName}</div>
+        <div className={styles.firstLetter}>{firstLetter}</div>
       </button>
+      {showDropDown && (
+        <section className={styles.popUp}>
+          <div className={styles.logOut} onClick={onLogout}>
+            <FiLogOut />
+            <span>Log out</span>
+          </div>
+          <div className={styles.owner}>by Yujin Won</div>
+        </section>
+      )}
     </section>
   );
 });
