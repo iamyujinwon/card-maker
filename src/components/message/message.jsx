19185d16c4e521fff12d6df2228755ec038b2330
Yujin Won <iamyujinwon@gmail.com>
2022-09-09 18:55:47 -0400

    delete message component

--- src/components/message/message.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './message.module.css';
-
-const Message = ({ msg }) => {
-  return <div className={styles.msg}>{msg}</div>;
-};
-
-export default Message;
